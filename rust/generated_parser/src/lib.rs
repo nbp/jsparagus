624cb94c79d050a1d3c960252210be214c16e505
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2019-10-18 16:05:08 +0200

    Add top-level lib.rs file for the generated parser.

---
+++ rust/generated_parser/src/lib.rs
@@ -0,0 +1,8 @@
+mod ast_builder;
+mod error;
+mod parser_tables_generated;
+mod stack_value_generated;
+pub use ast_builder::*;
+pub use error::*;
+pub use parser_tables_generated::*;
+pub use stack_value_generated::*;
